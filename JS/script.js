d96a392f213a6334738eccd579b1f7342d6dedc4
Shaleen Govil <shaleengovil2000@gmail.com>
2021-06-16 20:47:01 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,62 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\uf2ed");
+  span.className = "far";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// clicking on trash icon will delete the task node.
+var close = document.getElementsByClassName("far");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// li is brought to checked state.
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// creating a new task node.
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("No Task Added!");
+    } else {
+      document.getElementById("myUl").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\uf2ed");
+    span.className = "far";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+
+//   challange: to create a task node after enter is hit.
+function enterKey(event){
+    if(event.keyCode === 13){
+        newElement();
+    }
+}
 
